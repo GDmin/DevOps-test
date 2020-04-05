29d7118d987177dafeb03993015331dd039dc022
GDmin <minco.0418@gmail.com>
2020-04-05 18:16:32 +0800

    buildspec update v8

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ USERNAME=396654005341.dkr.ecr.us-east-1.amazonaws.com
 # image name
 IMAGE=devops-hw-5
 docker build -t $USERNAME/$IMAGE:latest .
-version='cat VERSION'
-echo "version: $version"
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
 docker push $USERNAME/$IMAGE:$version
 
