e1a034006c1d36b5312d8a5663fe79774557cdf3
GDmin <minco.0418@gmail.com>
2020-04-05 19:01:08 +0800

    buildspec update v13

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ USERNAME=396654005341.dkr.ecr.us-east-1.amazonaws.com
 # image name
 IMAGE=devops-hw-5
 docker build -t $USERNAME/$IMAGE:latest .
-docker push $USERNAME/$IMAGE:lastest
+docker push $USERNAME/$IMAGE:latest
 
