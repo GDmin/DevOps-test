1a3f52b1c8719876d13839092f997a4637dba533
GDmin <minco.0418@gmail.com>
2020-04-05 18:26:10 +0800

    buildspec update v10

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -ex
 #set registry
 USERNAME=396654005341.dkr.ecr.us-east-1.amazonaws.com
 # image name
-IMAGE=396654005341.dkr.ecr.us-east-1.amazonaws.com/devops-hw-5
+IMAGE=devops-hw-5
 docker build -t $USERNAME/$IMAGE:latest .
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
 docker push $USERNAME/$IMAGE:$version
